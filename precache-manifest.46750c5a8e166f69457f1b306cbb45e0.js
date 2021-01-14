30375346023be16e8e0868bb20d10e99cf5132cb
Daniel Gazcon <daniel.gazcon27@gmail.com>
2021-01-14 15:49:04 -0400

    Updates

---
+++ precache-manifest.46750c5a8e166f69457f1b306cbb45e0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a7b3df2c06af0c4653358c7c7d12a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "283d13ee6d8b4cf5b540",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "b603d6f98eb5446f8f24",
+    "url": "/static/css/main.7685329d.chunk.css"
+  },
+  {
+    "revision": "283d13ee6d8b4cf5b540",
+    "url": "/static/js/2.22a3dd46.chunk.js"
+  },
+  {
+    "revision": "d05b5611dac597c24d21b48fee393d7e",
+    "url": "/static/js/2.22a3dd46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b603d6f98eb5446f8f24",
+    "url": "/static/js/main.6c1cb650.chunk.js"
+  },
+  {
+    "revision": "28cef5f29134709242a7",
+    "url": "/static/js/runtime-main.238946b6.js"
+  },
+  {
+    "revision": "2fa8d2b2f933a0654da67eaadbca0113",
+    "url": "/static/media/5tenedores.2fa8d2b2.png"
+  },
+  {
+    "revision": "548e77e130ea884c764acabe1ba1ad63",
+    "url": "/static/media/Arame-Regular.548e77e1.ttf"
+  },
+  {
+    "revision": "885e5558c90864ddb72ad486f36470cc",
+    "url": "/static/media/RobotoCondensed-Light.885e5558.ttf"
+  },
+  {
+    "revision": "692c37377cd1e0f4850f47c7c0148426",
+    "url": "/static/media/VT323-Regular.692c3737.ttf"
+  },
+  {
+    "revision": "61e787d237a98cc1619d0773dd4e38cc",
+    "url": "/static/media/aduanalibre.61e787d2.png"
+  },
+  {
+    "revision": "45254f68cfbd18ddff9a7ce694efcf0c",
+    "url": "/static/media/delete1.45254f68.jpg"
+  },
+  {
+    "revision": "9b470fa4da675eb4a59b065fbb29e043",
+    "url": "/static/media/delete2.9b470fa4.jpg"
+  },
+  {
+    "revision": "73129a4352952fdc00fcc7e55ef7a52f",
+    "url": "/static/media/img_parallax3.73129a43.jpg"
+  },
+  {
+    "revision": "83727e5f46217a61e0a333c31f0075e3",
+    "url": "/static/media/loader.83727e5f.svg"
+  },
+  {
+    "revision": "d7fa17447a534ce950430be30498c46b",
+    "url": "/static/media/logo-delete.d7fa1744.png"
+  },
+  {
+    "revision": "5fd889765a2dda24f3b431af213c72f5",
+    "url": "/static/media/mf-avatar.5fd88976.svg"
+  }
+]);
